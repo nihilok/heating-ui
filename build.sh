a37c7a773a1d096eba0cdded81fd93c0e2e806cd
nihilok <mjfullstack@gmail.com>
2023-12-07 11:11:32 +0000

    Fix api url and update build script to check it in future

--- build.sh
+++ build.sh
@@ -1,4 +1,16 @@
+set -e
+
 DEST="../../Python/central-heating-api/application/front-end/"
+
+AUTH_URL=$(grep "const AUTH_URL" "$(pwd)/src/context/AuthContext.tsx" | awk -F'"' '{print $2}')
+
+if [[ "$AUTH_URL" == "http://localhost:8080" ]]; then
+  echo "AUTH_URL is set to localhost."
+  exit 1
+else
+  echo "AUTH_URL is not set to localhost."
+  exit 0
+fi
 
 npm run build
 mv "$DEST/manifest.webmanifest" ./
--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,8 +1,8 @@
 import * as React from "react";
 import { useBrowserStorage } from "../hooks/useBrowserStorage.ts";
 
-const AUTH_URL = "http://localhost:8080";
-// const AUTH_URL = window.location.origin;
+// const AUTH_URL = "http://localhost:8080";
+const AUTH_URL = window.location.origin;
 const API_URL = `${AUTH_URL}/api/v3`;
 
 type Token = string | null;
